4d8cef1bf4d8cfe05c5933207e337448c0305b2d
Patrick O'Malley <pomalley963@gmail.com>
2025-08-29 13:04:36 +0930

    Day 3: mDNS discovery, test, metrics

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use futures::prelude::*;
-use libp2p::{ping, swarm::{SwarmEvent}, Multiaddr};
+use libp2p::SwarmBuilder;
+use libp2p::{identity, mdns, noise, ping, swarm::{Swarm, SwarmEvent}, tcp, yamux, core::upgrade::Version, Multiaddr, Transport};
+use libp2p_swarm_derive::NetworkBehaviour;
 use serde::{Deserialize, Serialize};
 use std::error::Error;
 use std::fs::File;
@@ -26,9 +28,34 @@ fn load_note() -> io::Result<Note> {
     Ok(note)
 }
 
+#[derive(NetworkBehaviour)]
+#[behaviour(out_event = "CombinedEvent")]
+struct CombinedBehaviour {
+    ping: ping::Behaviour,
+    mdns: mdns::tokio::Behaviour,
+}
+
+#[derive(Debug)]
+enum CombinedEvent {
+    Ping(ping::Event),
+    Mdns(mdns::Event),
+}
+
+impl From<ping::Event> for CombinedEvent {
+    fn from(event: ping::Event) -> Self {
+        CombinedEvent::Ping(event)
+    }
+}
+
+impl From<mdns::Event> for CombinedEvent {
+    fn from(event: mdns::Event) -> Self {
+        CombinedEvent::Mdns(event)
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // Example storage test
+    // Storage test
     let note = Note {
         id: 1,
         title: "Test Note".to_string(),
@@ -38,30 +65,45 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let loaded = load_note()?;
     println!("Loaded note: {:?}", loaded);
 
-    // Existing P2P code (integrated)
-    let mut swarm = libp2p::SwarmBuilder::with_new_identity()
+    // P2P with mDNS
+    let local_key = identity::Keypair::generate_ed25519();
+    let local_peer_id = local_key.public().to_peer_id();
+    println!("Local peer id: {:?}", local_peer_id);
+
+    let transport = tcp::tokio::Transport::default()
+        .upgrade(Version::V1)
+        .authenticate(noise::Config::new(&local_key)?)
+        .multiplex(yamux::Config::default())
+        .boxed();
+
+    let behaviour = CombinedBehaviour {
+        ping: ping::Behaviour::default(),
+        mdns: mdns::tokio::Behaviour::new(mdns::Config::default(), local_peer_id)?,
+    };
+
+    let mut swarm = SwarmBuilder::with_existing_identity(local_key)
         .with_tokio()
-        .with_tcp(
-            libp2p::tcp::Config::default(),
-            libp2p::noise::Config::new,
-            libp2p::yamux::Config::default,
-        )?
-        .with_behaviour(|_| ping::Behaviour::default())?
-        .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(std::time::Duration::from_secs(30)))
+        .with_other_transport(|_| Ok(transport))?
+        .with_behaviour(|_| behaviour)?
         .build();
 
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
-    if let Some(addr) = std::env::args().nth(1) {
-        let remote: Multiaddr = addr.parse()?;
-        swarm.dial(remote)?;
-        println!("Dialed {addr}");
-    }
-
     loop {
         match swarm.select_next_some().await {
-            SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {address:?}"),
-            SwarmEvent::Behaviour(event) => println!("{event:?}"),
+            SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {:?}", address),
+            SwarmEvent::Behaviour(CombinedEvent::Ping(event)) => println!("Ping: {:?}", event),
+            SwarmEvent::Behaviour(CombinedEvent::Mdns(mdns::Event::Discovered(list))) => {
+                for (peer_id, addr) in list {
+                    println!("Discovered {} at {}", peer_id, addr);
+                    swarm.dial(addr.with(libp2p::multiaddr::Protocol::P2p(peer_id)))?;
+                }
+            }
+            SwarmEvent::Behaviour(CombinedEvent::Mdns(mdns::Event::Expired(list))) => {
+                for (peer_id, addr) in list {
+                    println!("Expired {} at {}", peer_id, addr);
+                }
+            }
             _ => {}
         }
     }
